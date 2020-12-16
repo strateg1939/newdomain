e8f8a8dacd7ccdd1871cb75e138b6effaa62cb16
Ivan <ikryvosheiev@gmail.com>
2020-12-16 19:36:07 +0200

    slightly changed send_data

--- js/subs.js
+++ js/subs.js
@@ -1,22 +1,20 @@
 function subs_send_data(){
 	var all_subscriptions =  document.getElementById("subscription_for_user").querySelectorAll(".option-input");
-	var topic;
-	var topic_id;
-	var send_data = {to_subscribe : [], to_unsubscribe : [],to_subscribe_id: [], to_unsubscribe_id : [], user_mail : params.mail};
+	var send_data = {to_subscribe : {}, to_unsubscribe : {}, user_mail : params.mail};
 	for(let i = 0; i < all_subscriptions.length; i++){
 		if(all_subscriptions[i].checked){
-			send_data.to_subscribe.push(all_subscriptions[i].value);
-			send_data.to_subscribe_id.push(all_subscriptions[i].name);
+			send_data.to_subscribe[all_subscriptions[i].name] = all_subscriptions[i].value;
 		}
 	}
 	var all_unsubscriptions =  document.getElementById("unsubscription_for_user").querySelectorAll(".option-input");
 	for(let i = 0; i < all_unsubscriptions.length; i++){
 		if(all_unsubscriptions[i].checked){
-			send_data.to_unsubscribe.push(all_unsubscriptions[i].value);
-			send_data.to_unsubscribe_id.push(all_unsubscriptions[i].name);
+			send_data.to_unsubscribe[all_unsubscriptions[i].name] = all_unsubscriptions[i].value;
 		}
 	}
-	if(send_data.to_subscribe.length === 0 && send_data.to_unsubscribe.length === 0){
+	var to_subscribe_length = Object.keys(send_data.to_subscribe).length;
+	var to_unsubscribe_length = Object.keys(send_data.to_unsubscribe).length;
+	if(to_subscribe_length === 0 && to_unsubscribe_length === 0){
 		document.getElementById("response_text").style.display = "none";
 		document.getElementById("response_text_hr").style.display = "none";
 		return;
@@ -27,24 +25,22 @@ function subs_send_data(){
 
 	ourRequest.onreadystatechange = function() {
 		if (ourRequest.status >= 200 && ourRequest.status < 400 && ourRequest.readyState === 4) {
-			if(ourRequest.responseText !== "success"){
+			if(ourRequest.responseText !== "\"success\""){
 				alert("Server error, try again later");
 				return;
 			}
 
 			document.getElementById("response_text").style.display = "inline";
 			document.getElementById("response_text_hr").style.display = "block";
-			for(let i = 0; i < send_data.to_subscribe.length; i++){
-				topic = send_data.to_subscribe[i];
-				topic_id = send_data.to_subscribe_id[i];
+			for(let topic_id in send_data.to_subscribe){
+				var topic = send_data.to_subscribe[topic_id];
 				document.getElementById(topic).remove();
 				document.getElementById(topic + ".text").remove();
 				document.getElementById("unsubscription_for_user").innerHTML += "<p><input type='checkbox' id = \'" + topic + "\'  class = 'option-input' value =  \'" + topic + "\' name = \'" + topic_id + "\' ><span class = 'checkbox_text' id = \'" + topic + ".text\' > " + topic + " </span></p>";
 
 			}    
-			for(let i = 0; i < send_data.to_unsubscribe.length; i++){
-				topic = send_data.to_unsubscribe[i];
-				topic_id = send_data.to_unsubscribe_id[i];
+			for(let topic_id in send_data.to_unsubscribe){
+				var topic = send_data.to_unsubscribe[topic_id];
 				document.getElementById(topic).remove();
 				document.getElementById(topic + ".text").remove();
 				document.getElementById("subscription_for_user").innerHTML += "<p><input type='checkbox' id = \'" + topic + "\'  class = 'option-input' value = \'" + topic + "\'  name = \'" + topic_id + "\'><span class = 'checkbox_text' id = \'" + topic + ".text\' > " + topic + " </span></p>";
@@ -75,14 +71,9 @@ function subs_send_data(){
 	    console.log("Connection error");
 	};
 	ourRequest.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-	if(send_data.to_subscribe.length !== 0 || send_data.to_unsubscribe.length !== 0){
-		ourRequest.send(JSON.stringify(send_data));
-	}
+	ourRequest.send(JSON.stringify(send_data));
 	
 }
-
-
-
 var subs_submit =  document.getElementById("subs_submit");
 if(subs_submit){
 	subs_submit.addEventListener("click", subs_send_data); 
